7df461042325fefee9610799c3244e96ef3a260b
estherkimyunjung <estherkimyunjung@gmail.com>
2021-02-24 02:05:37 -0600

    cset route for the app and create header

---
+++ src/components/Header.js
@@ -0,0 +1,86 @@
+import React from "react";
+import styled from "styled-components";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+
+function Header() {
+  return (
+    <Container>
+      <Main>
+        <AccessTimeIcon />
+        <SearchContainer>
+          <Search>
+            <input type="text" placeholder="Search..." />
+          </Search>
+        </SearchContainer>
+        <HelpOutlineIcon />
+      </Main>
+      <UserContainer>
+        <Name>Esther</Name>
+        <UserImage>
+          <img src="https://i.imgur.com/6VBx3io.png" alt="Avatar" />
+        </UserImage>
+      </UserContainer>
+    </Container>
+  );
+}
+
+export default Header;
+
+const Container = styled.div`
+  background: #350d36;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Main = styled.div`
+  display: flex;
+  margin-left: 16px;
+  margin-right: 16px;
+`;
+const SearchContainer = styled.div`
+  min-width: 400px;
+  margin-left: 16px;
+  margin-right: 16px;
+`;
+const Search = styled.div`
+  box-shadow: inset 0 0 0 1px rgb(104 74 104);
+  border-radius: 6px;
+  width: 100%;
+  // display: flex;
+  // align-items: center;
+
+
+  input {
+    background-color: transparent;
+    border: none;
+    padding-left: 8px;
+    padding-right: 8px
+    color: #fff;
+    padding-top: 4px;
+    padding-bottom: 4px;
+  }
+  input:focus {
+    outline: none;
+  }
+`;
+const UserContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding-right: 16px;
+  position: absolute;
+  right: 0;
+`;
+const Name = styled.div`
+  padding-right: 16px;
+`;
+const UserImage = styled.div`
+  width: 20px;
+  height: 20px;
+  border: 2px solid #fff;
+  border-radius: 3px;
+  img {
+    width: 100%;
+  }
+`;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Sidebar() {
+  return <div>SideBar</div>;
+}
+
+export default Sidebar;
