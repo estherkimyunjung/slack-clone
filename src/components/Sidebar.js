9b9caf2f022ccc891a590f873a221c6ae3599f7e
estherkimyunjung <estherkimyunjung@gmail.com>
2021-02-24 03:08:55 -0600

    create channels

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 import { sidebarItemData } from "../data/SidebarData";
+import AddIcon from "@material-ui/icons/Add";
 
 function Sidebar() {
   return (
@@ -20,6 +21,16 @@ function Sidebar() {
           </MainChannelItem>
         ))}
       </MainChannels>
+      <ChannelContainer>
+        <NewChannelContainer>
+          <div>channel</div>
+          <AddIcon />
+        </NewChannelContainer>
+        <ChannelList>
+          <Channel># Channel 1</Channel>
+          <Channel># Channel 2</Channel>
+        </ChannelList>
+      </ChannelContainer>
     </Container>
   );
 }
@@ -65,4 +76,34 @@ const MainChannelItem = styled.div`
   align-items: center;
   padding-left: 19px;
   cursor: pointer;
+  :hover {
+    background: #350d36;
+  }
+`;
+
+const ChannelContainer = styled.div`
+  color: rgb(188, 171, 188);
+  margin-top: 10px;
+`;
+const NewChannelContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 28px;
+  padding-left: 19px;
+  padding-right: 19px;
+`;
+const ChannelList = styled.div`
+  color: rgb(188, 171, 188);
+  height: 28px;
+`;
+const Channel = styled.div`
+  height: 28px;
+  display: flex;
+  align-items: center;
+  padding-left: 19px;
+  cursor: pointer;
+  :hover {
+    background: #350d36;
+  }
 `;
